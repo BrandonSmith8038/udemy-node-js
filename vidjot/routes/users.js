2e1ec5919c98a0d4f92ac78c4c29988b054af1a6
Brandon Smith <brandon@reddirtWebdesign.com>
2017-09-29 10:29:27 +0000

    Finished User Registration

--- vidjot/routes/users.js
+++ vidjot/routes/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
+// const passport = require("passport");
 const router = express.Router();
+
+//Load User Model
+require("../models/Users");
+const User = mongoose.model('users');
+
 
 // User Login Route
 router.get('/login', (req, res) => {
@@ -15,7 +22,7 @@ router.get('/register', (req, res) => {
 // Register Form POST
 router.post('/register', (req, res) => {
   let errors = [];
-  console.log(req.body)
+ 
 
   if(req.body.password != req.body.password2){
     errors.push({text:'Passwords do not match'});
@@ -35,7 +42,40 @@ router.post('/register', (req, res) => {
       password2: req.body.password2
     });
   } else {
-    res.send('passed');
+    
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(user){
+          req.flash('success_msg', 'Email Already Registered');
+          res.redirect('/users/register');
+         
+        } else {
+          const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    });
+    
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err) throw err;
+        newUser.password = hash;
+        newUser.save()
+          .then(user => {
+            req.flash('success_msg', 'You are now registered and can log in');
+            res.redirect('/users/login');
+          })
+          .catch(err => {
+            console.log(err);
+            return;
+          });
+      });
+    });
+    console.log(newUser);
+        }
+      })
+    
+    
   }
 });
 

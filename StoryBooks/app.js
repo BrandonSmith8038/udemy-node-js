bf3aec1ca906114e30d7283af44a2bc847cdb80f
Brandon Smith <brandon@reddirtwebdesign.com>
2017-09-30 08:47:53 +0000

    Finished Creating Google O-Auth Credentials

--- StoryBooks/app.js
+++ StoryBooks/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-  res.send('It Works!');
+  res.send('It Works!!!!');
 });
 
 
---
+++ StoryBooks/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://RedDirt:Nascar8038@ds155414.mlab.com:55414/storybooks-reddirt-dev',
+  googleClientID:'521431416873-kpk3n652dicub6d59uip7cisvef5f7e7.apps.googleusercontent.com',
+  googleClientSecret:'Lxak5bWD8Ehy2Gh4ZHZIJS2P'
+}

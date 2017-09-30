6a68a37c2e60e6e9b156b5011b4ac4ea061fb72c
Brandon Smith <brandon@reddirtwebdesign.com>
2017-09-30 07:46:37 +0000

    Story Books Init

---
+++ StoryBooks/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('It Works!');
+});
+
+
+
+
+
+
+
+const port = process.env.PORt || 5000
+
+app.listen(process.env.PORT,process.env.IP, () => {
+    console.log(`Server started at ${process.env.IP}:${process.env.PORT}`);
+});

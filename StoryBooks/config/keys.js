ba39565f06ebcfd056aa38a0644c7a9d00afb94e
Brandon Smith <brandon@reddirtwebdesign.com>
2017-10-05 10:04:12 +0000

    Finished Prep and Production Keys

--- StoryBooks/config/keys.js
+++ StoryBooks/config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:'mongodb://cowboy8038:Nascar8038@ds155414.mlab.com:55414/storybooks-reddirt-dev',
-  googleClientID:'521431416873-kpk3n652dicub6d59uip7cisvef5f7e7.apps.googleusercontent.com',
-  googleClientSecret:'Lxak5bWD8Ehy2Gh4ZHZIJS2P'
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require("./keys_prod")
+} else {
+  module.exports = require("./keys_dev")
 }
